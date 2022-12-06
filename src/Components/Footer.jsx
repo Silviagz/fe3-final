8b536808eb74bd75ade0d3e3146089266002ec0f
Silviagz <silviguardo15@gmail.com>
2022-12-06 18:50:37 -0500

    FinalFront3

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import { useContextGlobal } from './utils/global.context'
 
 const Footer = () => {
+  const {theme} = useContextGlobal();
+  console.log(theme.theme);
+
   return (
-    <footer>
+    <footer className='footer' id={theme.theme}>
         <p>Powered by</p>
-        <img src="./img/DH.png" alt='DH-logo' />
+        {theme.theme === "light" ? <img src="./images/DH.png" alt='DH-logo'/> : <img src="./images/DH2.png" alt='DH2-logo'/>}
     </footer>
   )
 }
 
-export default Footer
+
+export default Footer;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { useContextGlobal } from './utils/global.context'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Navbar = () => {
 
+  const {dispatch, theme} = useContextGlobal()
+
   return (
-    <nav>
+    <nav className='nav' id={theme.theme}>
       {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
       {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
-      <button>Change theme</button>
+      <div className='links-navbar'>
+        <Link to='/'>Home</Link>
+        <Link to='/contact'>Contact</Link>
+        <Link to='/favs'>Favs</Link>
+      </div>
+      <button className='btn-theme' onClick={() => dispatch({type: "theme"})}>🌞Change theme🌜</button>
     </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
